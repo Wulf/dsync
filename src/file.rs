9e6405f46702edc7941fc1d5d2c439fcbd7ad0ae
jean-santos <ewqjean@gmail.com>
2023-01-22 00:26:16 -0300

    cargo fmt (#3)
    * cargo fmt
    
    * fixups

--- src/file.rs
+++ src/file.rs
@@ -2,7 +2,7 @@ use std::path::PathBuf;
 
 pub struct MarkedFile {
     pub file_contents: String,
-    pub path: PathBuf
+    pub path: PathBuf,
 }
 
 impl MarkedFile {
@@ -14,7 +14,7 @@ impl MarkedFile {
                 "".to_string()
             } else {
                 std::fs::read_to_string(&path).expect(&format!("Could not read '{:#?}'", path))
-            }
+            },
         }
     }
 
@@ -31,7 +31,8 @@ impl MarkedFile {
         if self.file_contents.len() > 0 {
             self.file_contents.push('\n');
         }
-        self.file_contents.push_str(&format!("pub use {use_name};\n"))
+        self.file_contents
+            .push_str(&format!("pub use {use_name};\n"))
     }
 
     pub fn add_mod_stmt(&mut self, mod_name: &str) {
@@ -39,20 +40,29 @@ impl MarkedFile {
         if self.file_contents.len() > 0 {
             self.file_contents.push('\n');
         }
-        self.file_contents.push_str(&format!("pub mod {mod_name};\n"))
+        self.file_contents
+            .push_str(&format!("pub mod {mod_name};\n"))
     }
 
     pub fn remove_use_stmt(&mut self, mod_name: &str) {
         let content_to_remove = &format!("pub use {mod_name};");
         if self.file_contents.contains(content_to_remove) {
-            self.file_contents = self.file_contents.replace(content_to_remove, "").trim().to_string();
+            self.file_contents = self
+                .file_contents
+                .replace(content_to_remove, "")
+                .trim()
+                .to_string();
         }
     }
 
     pub fn remove_mod_stmt(&mut self, mod_name: &str) {
         let content_to_remove = &format!("pub mod {mod_name};");
         if self.file_contents.contains(content_to_remove) {
-            self.file_contents = self.file_contents.replace(content_to_remove, "").trim().to_string();
+            self.file_contents = self
+                .file_contents
+                .replace(content_to_remove, "")
+                .trim()
+                .to_string();
         }
     }
 
@@ -73,7 +83,10 @@ impl MarkedFile {
         // the whole purpose of file signatures is to prevent writing to files which aren't generated
         // and if a file's length is 0, then it's safe to write to this file!
         // :)
-        self.file_contents.len() == 0 || self.file_contents.starts_with(crate::parser::FILE_SIGNATURE)
+        self.file_contents.len() == 0
+            || self
+                .file_contents
+                .starts_with(crate::parser::FILE_SIGNATURE)
     }
 
     pub fn ensure_file_signature(&self) {
@@ -83,11 +96,14 @@ impl MarkedFile {
     }
 
     pub fn write(&self) {
-        std::fs::write(&self.path, &self.file_contents).expect(&format!("Could not write to file '{:#?}'", self.path));
+        std::fs::write(&self.path, &self.file_contents)
+            .expect(&format!("Could not write to file '{:#?}'", self.path));
     }
 
     pub fn delete(self) {
-        std::fs::remove_file(&self.path).expect(&format!("Could not delete redundant file '{:#?}'", self.path));
+        std::fs::remove_file(&self.path).expect(&format!(
+            "Could not delete redundant file '{:#?}'",
+            self.path
+        ));
     }
 }
-
