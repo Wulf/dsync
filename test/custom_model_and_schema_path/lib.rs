b476ae243897cbcab303d0318c6a041a8b813e75
hasezoey <hasezoey@gmail.com>
2023-11-02 18:49:06 +0100

    test: change "custom_model_and_schema_path" to be compile-able

---
+++ test/custom_model_and_schema_path/lib.rs
@@ -0,0 +1,6 @@
+#![allow(non_snake_case)]
+pub mod data;
+
+pub mod diesel {
+    pub use diesel::*;
+}
--- test/custom_model_and_schema_path/test.sh
+++ test/custom_model_and_schema_path/test.sh
@@ -4,4 +4,5 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 cd $SCRIPT_DIR
 
-cargo run -- -i data/schema.rs -o data/models -g id -g created_at -g updated_at -c "diesel::r2d2::PooledConnection<diesel::r2d2::ConnectionManager<diesel::pg::PgConnection>>" --schema-path "crate::data::schema::" --model-path "crate::data::models::"
+cargo run --manifest-path ../../Cargo.toml -- \
+-i data/schema.rs -o data/models -g id -g created_at -g updated_at -c "diesel::r2d2::PooledConnection<diesel::r2d2::ConnectionManager<diesel::pg::PgConnection>>" --schema-path "crate::data::schema::" --model-path "crate::data::models::"
