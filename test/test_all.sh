cb01abe3ecd2c72bf8e44111a70a858b51701e86
hasezoey <hasezoey@gmail.com>
2024-01-02 17:11:40 +0100

    chore(test): add new "test_all"
    to execute all "test_*" files in order

---
+++ test/test_all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This script is to run "dsync" generation on all test-cases
+
+# fail on non-0 exit codes, which makes it more obvious if a test has failed
+set -e
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+cd $SCRIPT_DIR
+
+echo "Testing Generation"
+./test_generate.sh
+
+# extra separator
+echo ""
+
+echo "Testing Compiliation"
+./test_compile.sh
