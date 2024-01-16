05d60709aa43e08cc7aa9267f7bc47003f5c0fad
hasezoey <hasezoey@gmail.com>
2024-01-16 13:52:26 +0100

    fix(code): correct the indent for generated "filter" function code
    re #126

--- src/code.rs
+++ src/code.rs
@@ -571,20 +571,20 @@ fn build_table_fns(
                     // "Option::None" will never match anything, and "is_null" is required to be used, see https://docs.diesel.rs/master/diesel/expression_methods/trait.ExpressionMethods.html#method.eq
                     format!(
                         r##"
-            if let Some(filter_{column_name}) = filter.{column_name}.clone() {{
-                query = if filter_{column_name}.is_some() {{ 
-                    query.filter({schema_path}{table_name}::{column_name}.eq(filter_{column_name}))
-                }} else {{
-                    query.filter({schema_path}{table_name}::{column_name}.is_null())
-                }};
-            }}"##
+        if let Some(filter_{column_name}) = filter.{column_name}.clone() {{
+            query = if filter_{column_name}.is_some() {{ 
+                query.filter({schema_path}{table_name}::{column_name}.eq(filter_{column_name}))
+            }} else {{
+                query.filter({schema_path}{table_name}::{column_name}.is_null())
+            }};
+        }}"##
                     )
                 } else {
                     format!(
                         r##"
-            if let Some(filter_{column_name}) = filter.{column_name}.clone() {{
-                query = query.filter({schema_path}{table_name}::{column_name}.eq(filter_{column_name}));
-            }}"##
+        if let Some(filter_{column_name}) = filter.{column_name}.clone() {{
+            query = query.filter({schema_path}{table_name}::{column_name}.eq(filter_{column_name}));
+        }}"##
                     )
                 }
             })
